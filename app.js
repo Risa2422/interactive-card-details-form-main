f8f6722bf6f9cbb7b1a7172e4fae5661b46ca65f
Risa Yamamoto <yamari2422@gmail.com>
2024-06-10 19:20:29 -0700

    commited for now

--- app.js
+++ app.js
@@ -1,66 +1,70 @@
 const cardForm = document.querySelector(".card-form");
 const holderName = document.querySelector("#holder-name");
 const cardNumber = document.querySelector("#card-number");
-const expDate = document.querySelector("#exp-date-label");
+const expDates = document.querySelectorAll("#exp-date-label");
 const cvc = document.querySelector("#cvc-number");
 const submitButton = document.querySelector("#button");
+
+const month = document.querySelector(".month");
+const year = document.querySelector(".year");
 
 const holderNameLine = document.querySelector(".holder-name-line");
 const cardNumberLine = document.querySelector(".card-number-line");
 const expDateLine = document.querySelector(".exp-date-line");
 const cvcLine = document.querySelector(".cvc-line");
 
-const submit = document.querySelector(".confirm");
+const submit = document.querySelector("#confirm");
 
 const blankErrorMessage = "can't be blank";
 const formatErrorMessage = "Wrong format";
 
-submit.addEventListener("click", () => {
+cardForm.addEventListener("submit", (e) => {
+  e.preventDefault();
   // cardholder name
   let numericNumber = /^[0-9]+$/;
   let numericCharacter = /\d/;
 
   if (holderName.value === "") {
-    showBlankErrMessage(holderName, holderNameLine);
+    showErrMessage(holderName, holderNameLine);
     holderNameLine.innerText = blankErrorMessage;
   } else if (
     numericCharacter.test(holderName.value) ||
     numericNumber.test(holderName.value)
   ) {
-    showBlankErrMessage(holderName, holderNameLine);
+    showErrMessage(holderName, holderNameLine);
     holderNameLine.innerText = formatErrorMessage;
   }
 
-  // cord number
+  // card number
   if (cardNumber.value === "") {
-    showBlankErrMessage(cardNumber, cardNumberLine);
+    showErrMessage(cardNumber, cardNumberLine);
     cardNumberLine.innerText = blankErrorMessage;
   } else if (!numericNumber.test(cardNumber.value)) {
-    showBlankErrMessage(cardNumber, cardNumberLine);
+    showErrMessage(cardNumber, cardNumberLine);
     cardNumberLine.innerText = formatErrorMessage;
   }
 
-  // exp date
-  // loop?
-  if (expDate.value === "") {
-    showBlankErrMessage(expDate, expDateLine);
+  if (month.value == "") {
+    showErrMessage(month, expDateLine);
     expDateLine.innerText = blankErrorMessage;
-  } else if (!numericNumber.test(cardNumber.value)) {
-    showBlankErrMessage(expDate, expDateLine);
-    expDateLine.innerText = formatErrorMessage;
   }
 
-  // cvc
+  if (year.value == "") {
+    showErrMessage(year, expDateLine);
+    expDateLine.innerText = blankErrorMessage;
+  }
+
   if (cvc.value === "") {
-    showBlankErrMessage(cvc, cvcLine);
+    // cvc
+    showErrMessage(cvc, cvcLine);
     cvcLine.innerText = blankErrorMessage;
   } else if (!numericNumber.test(cardNumber.value)) {
-    showBlankErrMessage(cvc, cvcLine);
+    showErrMessage(cvc, cvcLine);
     cvcLine.innerText = formatErrorMessage;
   }
 });
 
-function showBlankErrMessage(info, line) {
+function showErrMessage(info, line) {
   info.classList.add("error-form");
   line.classList.add("error-message");
   submitButton.disabled = true;
